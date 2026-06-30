c381d093773bc8fe5e2554940874d1a100469d18
Angelin J <jenefa.angelin.j@accenture.com>
2026-06-30 12:13:03 +0530

    Fix db json import path

--- src/seed.ts
+++ src/seed.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import jobsData from "../../db.json";
+import jobsData from "../db.json";
 import { Job } from "./models/Job";
 
 dotenv.config();
